cae3fbffd9ba7d51fdac3c7efff32716ee424692
corvuscoraxprincipalis <64034236+corvuscoraxprincipalis@users.noreply.github.com>
2020-04-25 20:57:39 +0200

    Update multi-location.js

--- SmartCity_AR/js/multi-location.js
+++ SmartCity_AR/js/multi-location.js
@@ -13,8 +13,8 @@ function staticLoadPlaces() {
         {
             name: 'Pokèmon',
             location: {
-                // lat: <your-latitude>,
-                // lng: <your-longitude>,
+                lat: 47.488783,
+                lng: 19.059657,
             },
         },
     ];
